da90fece57e75a230a51ef3bb9f612558a05d2fa
anggara setiawan <aanggaadc@gmail.com>
2025-12-07 13:12:15 +0700

    refactor main tsx, create layout for app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./pages/home";
+import TopNavHero from "./components/templates/top-hero.";
+import Text from "./components/atoms/text";
+
+import "material-symbols";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  const { lang } = useParams();
+  const currentLang = lang || "ko-KR";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <html lang={currentLang} className="font-poppins">
+      <body>
+        <main className="bg-slight_light">
+          <header>
+            <TopNavHero />
+          </header>
+
+          {children}
+
+          <footer className="pt-9 pb-[60px] md:pt-12 md:pb-20 px-4 max-w-[1201.5px] mx-auto">
+            <Text variant="lightest_dark" className="text-[13px] font-black">
+              &copy; {new Date().getFullYear()} Hyperhire
+            </Text>
+          </footer>
+        </main>
+      </body>
+    </html>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/:lang?"
+          element={
+            <Layout>
+              <PageWrapper />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function PageWrapper() {
+  return (
+    <Routes>
+      <Route path="/:lang?" element={<Home />} />
+    </Routes>
+  );
+}
